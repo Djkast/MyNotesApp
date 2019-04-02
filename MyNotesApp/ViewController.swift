ce7f6c80a8f46ac5da613c191cceaaa31c2afb15
omar uriel R.C <omaruriel181@gmail.com>
2019-04-02 16:59:54 -0600

    MyNotesApp

--- MyNotesApp/ViewController.swift
+++ MyNotesApp/ViewController.swift
@@ -10,16 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtTitle: UITextField!
+    
+    @IBOutlet weak var txtDes: UITextView!
+    
+    var EditNote:MyNotes?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        if let note = EditNote {
+            txtTitle.text = note .title
+            txtDes.text = note.details
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func buSave(_ sender: Any) {
+        var newNote:MyNotes?
+        if let note = EditNote {
+            newNote = note
+        } else {
+            newNote = MyNotes(context: context)
+        }
+        newNote?.title = txtTitle.text
+        newNote?.details = txtDes.text
+        newNote?.date_save = NSDate() as Date
+        do{
+            ad.saveContext()
+            txtTitle.text = ""
+            txtDes.text = ""
+        } catch {
+            print("Cannot save")
+        }
     }
-
-
+    
+    
+    @IBAction func buBack(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
 }
 
